8c052c18ef8d837c54ecfedd4832abadc6322f67
sarakara <gulcinsarankara@hotmail.com>
2022-10-28 11:52:13 -0400

    Spring Data-Query

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -0,0 +1,38 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Course;
+import com.sun.jdi.connect.ListeningConnector;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    //find all courses by category select (* from courses) where category=..
+
+    List<Course> findByCategory(String category);
+
+    //find all courses by category and order the entities by name
+
+    List<Course> findByCategoryOrderByName(String category);
+
+    //check if a course with the supplied name exists. Return true if exists, false otherwise
+
+    boolean existsByName(String name);
+
+    //Return the count of courses for given category
+
+    int countByCategory(String category);
+
+    //find all the courses that start with the given course name string
+
+    List<Course> findByNameStartingWith(String name);
+
+    //find all courses by category and return a stream
+
+    Stream<Course> streamAllByCategory(String category);
+
+
+
+}
