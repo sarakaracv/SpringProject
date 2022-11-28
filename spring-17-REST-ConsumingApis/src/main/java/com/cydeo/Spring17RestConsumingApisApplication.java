c854c4dc7855ecbc5644d41664d66991c9d6cb0b
sarakara <gulcinsarankara@hotmail.com>
2022-11-28 15:41:27 -0500

    api

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -3,13 +3,22 @@ package com.cydeo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Spring15RestApplication {
+@EnableFeignClients
+public class Spring17RestConsumingApisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring15RestApplication.class, args);
+
+        SpringApplication.run(Spring17RestConsumingApisApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
     @Bean
---
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/client/DemoController.java
@@ -0,0 +1,98 @@
+package com.cydeo.client;
+
+import com.cydeo.dto.GenreDTO;
+import com.cydeo.dto.MovieCinemaDTO;
+import com.cydeo.entity.Genre;
+import com.cydeo.entity.MovieCinema;
+import com.cydeo.repository.GenreRepository;
+import com.cydeo.repository.MovieCinemaRepository;
+import com.cydeo.service.GenreService;
+import com.cydeo.service.MovieCinemaService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class DemoController {
+
+    private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
+
+    private final MovieCinemaService movieCinemaService;
+    private final GenreService genreService;
+
+    public DemoController(MovieCinemaService movieCinemaService, GenreService genreService) {
+        this.movieCinemaService = movieCinemaService;
+        this.genreService = genreService;
+    }
+
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinemaDTO> readAllCinemaFlux() {
+
+        return Flux.fromIterable(movieCinemaService.findAll());
+
+    }
+
+//    @GetMapping("/mono-movie-cinema/{id}")
+//    public Mono<MovieCinema> readById(@PathVariable("id") Long id){
+//
+//        return Mono.just(movieCinemaRepository.findById(id).get());
+//
+//    }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public ResponseEntity<Mono<MovieCinemaDTO>> readById(@PathVariable("id") Long id) {
+
+        return ResponseEntity.ok(Mono.just(movieCinemaService.findById(id)));
+
+    }
+
+    @PostMapping("/create-genre")
+    public Mono<GenreDTO> createGenre(@RequestBody GenreDTO genre) {
+
+        GenreDTO createdGenre = genreService.save(genre);
+
+        return Mono.just(createdGenre);
+//        return Mono.just(genreRepository.save(genre));
+
+    }
+
+    @DeleteMapping("/delete/genre/{id}")
+    public Mono<Void> deleteGenre(@PathVariable("id") Long id) {
+
+        genreService.deleteById(id);
+
+        return Mono.empty();
+    }
+
+
+//    ---------------------------WEBCLIENT---------------------------
+
+
+    @GetMapping("/flux")
+    public Flux<MovieCinema> readWithWebClient() {
+
+        return webClient
+                .get()
+                .uri("/flux-movie-cinemas")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToFlux(MovieCinema.class);
+
+    }
+
+
+    @GetMapping("/mono/{id}")
+    public Mono<MovieCinema> readMonoWithWebClient(@PathVariable("id") Long id) {
+
+        return webClient
+                .get()
+                .uri("/mono-movie-cinema/{id}", id)
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
+
+    }
+}
